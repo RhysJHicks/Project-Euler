4aaf9ee7d22b46bb3d5ed4137c4ccac536aace96
RhysJHicks <151779160+RhysJHicks@users.noreply.github.com>
2026-06-16 19:49:19 +0800

    Changes

--- Problems/Problem 5 - Smallest Multiple.py
+++ Problems/Problem 5 - Smallest Multiple.py
@@ -1,13 +1,22 @@
 # Problem 5: Smallest Multiple
+# Answer: 232792560
 # Problem Specification: Smallest Possible divisible of all numbers from 1 to 20, This is the same as what is common multiple.
 # Approach 1: Brute Force
 
 
 def approach_1():
-    nums = range(1, 11)
-    while True:
-        for i in nums:
-            if i % 2 != 0:
-                continue
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+    def lcm(a, b):
+        return (a * b) // gcd(a, b)
+
+    number = 1
+    for i in range(1, 21):
+        number = lcm(number, i)
+
+    print(number)
 
 approach_1()
